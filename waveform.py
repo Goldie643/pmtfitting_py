ed26c907bcac6d7e4469ef7105869be808f8cb45
goldie643 <alex.goldsack1@gmail.com>
2023-04-03 16:43:54 +0100

    Process multiple files.

--- waveform.py
+++ waveform.py
@@ -1,31 +1,48 @@
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
+from glob import glob
 from sys import argv
 
-in_file = argv[1]
+# Use glob to expand wildcards
+fnames = []
+for arg in argv[1:]:
+    fnames += glob(arg)
 
-print("Parsing XML...", end="")
-tree = ET.parse(in_file)
-print(" done!")
-print("Getting root...", end="")
-root = tree.getroot()
-print(" done!")
+def combine_wforms(fname):
+    """
+    Opens CAEN digitizer XML output.
+    Combines traces to to return total and average waveform
 
-wform_tot = None
-i = 0
-for child in root.iter("event"):
-    # print(child.tag, child.attrib)
-    wform = np.array(child.find("trace").text.split()).astype(int)
-    if wform_tot is None:
-        wform_tot = wform
-    else:
-        wform_tot += wform
-    i += 1
-    if (i>10): break
+    :param str fname: filename of input XML.
+    """
+    print("Processing file %s, parsing XML..." % fname)
+    tree = ET.parse(fname)
+    root = tree.getroot()
+    print("... done!")
 
-wform_avg = wform_tot/i
+    print("Processing waveforms...")
+    wform_tot = None
+    # Loops through every "event" in the XML, each with a "trace" (waveform)
+    for i,child in enumerate(root.iter("event")):
+        # print(child.tag, child.attrib)
+        # Use int as dtype to ensure numpy arithmetic
+        wform = np.array(child.find("trace").text.split()).astype(int)
 
-plt.plot(range(len(wform_avg)), wform_avg)
+        # First wform case
+        if wform_tot is None:
+            wform_tot = wform
+        else:
+            wform_tot += wform
+
+    wform_avg = wform_tot/i
+    print("... done! %i waveforms processed" % i)
+
+    return (wform_avg, wform_tot)
+
+for fname in fnames:
+    wform_avg, wvform_tot = combine_wforms(fname)
+    plt.plot(range(len(wform_avg)), wform_avg, label=fname)
 # plt.plot(range(len(wform_tot)), wform_tot)
+plt.legend()
 plt.show()
