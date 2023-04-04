b2f8ca51b7c1d0da8d512df8da783399c0642d93
goldie643 <alex.goldsack1@gmail.com>
2023-04-04 15:06:46 +0100

    Saves waveforms to pickle once loaded from XML.

--- waveform.py
+++ waveform.py
@@ -1,46 +1,65 @@
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 from glob import glob
 from sys import argv
+from os.path import splitext
 
 # Use glob to expand wildcards
 fnames = []
 for arg in argv[1:]:
     fnames += glob(arg)
 
+print(fnames)
+
 def combine_wforms(fname):
     """
     Opens CAEN digitizer XML output.
-    Combines traces to to return total and average waveform
+    Combines traces to to return average waveform.
 
     :param str fname: filename of input XML.
     """
     print("File: %s" % fname)
+    split_fname = splitext(fname)
+
+    # If pickle file is passed just load wform list from that
+    if split_fname[1] == ".pkl":
+        print("Loading from Pickle...")
+        with open(fname, "rb") as f:
+            wforms = pickle.load(f)
+        wform_avg = sum(wforms)/len(wforms)
+        print("... done!")
+        return wform_avg
+
     print("Parsing XML...")
     tree = ET.parse(fname)
     root = tree.getroot()
     print("... done!")
 
     print("Processing waveforms...")
-
     wforms = []
     # Loops through every "event" in the XML, each with a "trace" (waveform)
     for i,child in enumerate(root.iter("event")):
-        # Trace is spaced wform values, split on spaces and convert to np array.
+        # Trace is spaced wform values. Split on spaces and convert to np array.
         # Use int as dtype to ensure numpy arithmetic.
         wforms.append(np.array(child.find("trace").text.split()).astype(int))
+        if i>10: break
         if (i % 100) == 0:
             print("    %i processed...\r" % i, end="")
 
-    wform_tot = sum(wforms)
-    wform_avg = wform_tot/i
-    print("... done! %i waveforms processed" % i)
+    # Dump waveforms to pickle (quicker than parsing XML each time)
+    pickle_fname = split_fname[0]+".pkl"
+    with open(pickle_fname, "wb") as f:
+        pickle.dump(wforms, f)
 
-    return (wform_avg, wform_tot)
+    wform_avg = sum(wforms)/len(wforms)
+    print("... done! %i waveforms processed." % i)
+
+    return wform_avg
 
 for fname in fnames:
-    wform_avg, wform_tot = combine_wforms(fname)
+    wform_avg = combine_wforms(fname)
     plt.plot(range(len(wform_avg)), wform_avg, label=fname)
 plt.legend()
 plt.show()
